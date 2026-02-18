3d2c36af39ebfab8a4595ee0e0b15cdcf6d532a0
BHUVAN <cnbhuvan011@gmail.com>
2026-02-18 16:53:33 +0530

    Fix: Add missing face_recognition_model-shard2 required by manifest

--- download_models.js
+++ download_models.js
@@ -14,6 +14,7 @@ const models = [
     "face_landmark_68_model-shard1",
     "face_recognition_model-weights_manifest.json",
     "face_recognition_model-shard1",
+    "face_recognition_model-shard2",
     "tiny_face_detector_model-weights_manifest.json",
     "tiny_face_detector_model-shard1"
 ];
